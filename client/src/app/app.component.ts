fd24aa053554b6bf4ef92989d7c4f693c4660c05
Blaine DeYoung <45204685+blainedeyoung@users.noreply.github.com>
2023-09-07 04:17:41 -0400

    added search, sort, and filtering to store front

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,10 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit{
   title = 'SkiNet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product>[]>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: any) => {
-        // console.log(response);
-        this.products = response.data
-      },
-      error: error => console.log(error),
-      complete: () => console.log('request completed')
-    })
+    
   }
 }
